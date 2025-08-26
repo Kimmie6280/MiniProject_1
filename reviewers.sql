1c75f2db3c14aec12f155bf0f83bdb36d660bd72
Kimberly Ramgopal <66437777+Kimmie6280@users.noreply.github.com>
2025-08-26 15:04:30 -0400

    Update reviewers.sql

--- reviewers.sql
+++ reviewers.sql
@@ -1,1 +1,33 @@
+/* Create a second table that provides at least two user
+reviews for each of at least two of the videos. These should be imaginary reviews that include
+columns for the user’s name (“Asher”, “John”, etc.), the rating (which could be NULL, or a
+number between 0 and 5), and a short text review (“Loved it!”). There should be a column that
+links back to the ID column in the table of videos.*/
+
+/*Create and populate Reviewers table.*/
+
+/* Create a second table that provides at least two user
+reviews for each of at least two of the videos. These should be imaginary reviews that include
+columns for the user’s name (“Asher”, “John”, etc.), the rating (which could be NULL, or a
+number between 0 and 5), and a short text review (“Loved it!”). There should be a column that
+links back to the ID column in the table of videos.*/
+
+
+create table reviewers(
+review_id serial unique primary Key,
+fname varchar(50),
+lname varchar(50),
+rating int,
+videoID int
+)
+
+select * from reviewers;
+
+insert into reviewers(fname,lname,rating,videoID)
+values 
+('Kimberly','Ramgopal',3,2),
+('Nicholas','Ramgopal',1,1),
+('Brandon','Ramgopal',5,3)
+
+
 
